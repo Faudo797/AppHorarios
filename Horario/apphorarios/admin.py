bf80cb5d86cafbbc11871235290f4425d8a2ad2e
Fernando <fernandoaaron072@gmail.com>
2025-05-21 17:32:10 -0500

    cambiosa models, admin y views

--- Horario/apphorarios/admin.py
+++ Horario/apphorarios/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Estudiante, Profesor, Aula, Clase
+from .models import (
+    Aula,
+    Estudiante,
+    Profesor,
+    Clase,
+    Asignatura,
+    Hora,
+    Grado
+)
 
 admin.site.register(Aula)
 admin.site.register(Estudiante)
 admin.site.register(Profesor)
 admin.site.register(Clase)
+admin.site.register(Asignatura)
+admin.site.register(Hora)
+admin.site.register(Grado)
--- Horario/apphorarios/views.py
+++ Horario/apphorarios/views.py
@@ -1,26 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Estudiante
-from .models import Profesor
-from .models import Aula
-from .models import Clase
+from .models import Aula, Estudiante, Profesor, Clase, Asignatura, Hora, Grado
 
 def inicio(request):
     return HttpResponse("¡Hola desde mi app Django!")
 
+
 def lista_estudiantes(request):
-    estudiantes = Estudiante.objects.all() 
+    estudiantes = Estudiante.objects.all()
     return render(request, 'mi_app/lista_estudiantes.html', {'estudiantes': estudiantes})
 
+
 def lista_profesores(request):
-    profesores = Profesor.objects.all()  # Obtener todos los profesores
+    profesores = Profesor.objects.all()
     return render(request, 'mi_app/lista_profesores.html', {'profesores': profesores})
 
-# Vista para mostrar todas las aulas
+
 def lista_aulas(request):
-    aulas = Aula.objects.all()  # Obtener todas las aulas
+    aulas = Aula.objects.all()
     return render(request, 'mi_app/lista_aulas.html', {'aulas': aulas})
+
 
 def lista_clases(request):
     clases = Clase.objects.all()
-    return render(request, 'clases.html', {'clases': clases})
+    return render(request, 'mi_app/lista_clases.html', {'clases': clases})
+
+
+def lista_asignaturas(request):
+    asignaturas = Asignatura.objects.all()
+    return render(request, 'mi_app/lista_asignaturas.html', {'asignaturas': asignaturas})
+
+
+def lista_horas(request):
+    horas = Hora.objects.all()
+    return render(request, 'mi_app/lista_horas.html', {'horas': horas})
+
+
+def lista_grados(request):
+    grados = Grado.objects.all()
+    return render(request, 'mi_app/lista_grados.html', {'grados': grados})
