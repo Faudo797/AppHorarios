4117ca03efade06386a841f37a9125f76da9c921
Faudo797 <fernandoaaron072@gmail.com>
2026-06-12 23:22:39 -0500

    Update settings.py

--- AppHorarios-main/Horario/Horario/settings.py
+++ AppHorarios-main/Horario/Horario/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = 'django-insecure-g6c==##+1v(1srydmx8n$+xk#fk@f_g(et&lj$-7@^9wv#rvqd'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['apphorarios.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
